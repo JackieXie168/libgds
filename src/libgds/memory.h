e343afbcc054b293902d1d1126a822b61ee7dc85
bqu <bqu@8e8141a4-5d17-0410-8ace-bc53a4168d8f>
2004-06-14 07:35:20 +0000

    *** empty log message ***

--- src/libgds/memory.h
+++ src/libgds/memory.h
@@ -1,15 +1,23 @@
 // ==================================================================
 // @(#)memory.h
 //
-// @author Bruno Quoitin (bqu@infonet.fundp.ac.be)
+// @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 29/11/2002
-// @lastdate 30/11/2002
+// @lastdate 26/05/2004
 // ==================================================================
 
 #ifndef __MEMORY_H__
 #define __MEMORY_H__
 
 #include <stdlib.h>
+
+#include <libgds/types.h>
+
+#define MEM_FLAG_WARN_LEAK 0x01 /* Display a warning in case of memory
+				   leak when the memory.o object is
+				   destroyed (note that memory.o must
+				   be the last object in the .DTOR
+				   list) */
 
 extern unsigned long uAllocCount;
 
@@ -19,6 +27,12 @@ extern void * memalloc(size_t size);
 extern void * memrealloc(void * pPtr, size_t size);
 // ----- memfree ----------------------------------------------------
 extern void memfree(void * pPtr);
+// ----- mem_alloc_cnt ----------------------------------------------
+extern unsigned long int mem_alloc_cnt();
+// ----- mem_flag_set -----------------------------------------------
+extern void mem_flag_set(uint8_t uFlag, int iState);
+// ----- mem_flag_get -----------------------------------------------
+extern int mem_flag_get(uint8_t uFlag);
 
 #define MALLOC(s) memalloc(s)
 #define REALLOC(p, s) memrealloc(p, s)
--- src/libgds/types.h
+++ src/libgds/types.h
@@ -1,20 +1,31 @@
 // ==================================================================
 // @(#)types.h
 //
-// @author Bruno Quoitin (bqu@infonet.fundp.ac.be)
+// @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 24/11/2002
-// @lastdate 14/08/2003
+// @lastdate 31/05/2004
 // ==================================================================
 
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
-#ifdef __i386__
-#include <inttypes.h>
-#endif
+#include <libgds/config.h>
 
-#ifdef __sparc64__
-#include <sys/types.h>
+/* `inttypes.h' vs. `stdint.h' (quoting from the GNU autoconf manual)
+ *
+ * Paul Eggert notes that: ISO C 1999 says that `inttypes.h' includes
+ * `stdint.h', so there's no need to include `stdint.h' separately in
+ * a standard environment. Many implementations have `inttypes.h' but
+ * not `stdint.h' (e.g., Solaris 7), but I don't know of any
+ * implementation that has `stdint.h' but not `inttypes.h'. Nor do I
+ * know of any free software that includes `stdint.h'; `stdint.h'
+ * seems to be a creation of the committee. */
+#if HAVE_INTTYPES_H
+# include <inttypes.h>
+#else
+# if HAVE_STDINT_H
+#  include <stdint.h>
+# endif
 #endif
 
 #include <limits.h>
