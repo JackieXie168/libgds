b450e188081afbd1b7cf1235119d6296c58b4efc
bqu <bqu@8e8141a4-5d17-0410-8ace-bc53a4168d8f>
2006-04-10 08:45:59 +0000

    Changed: - Allow to log to a callback

--- src/libgds/log.c
+++ src/libgds/log.c
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be), Sebastien Tandel
 // @date 17/05/2005
-// @lastdate 24/01/2005
+// @lastdate 03/03/2006
 // ==================================================================
 
 #include <assert.h>
@@ -16,37 +16,66 @@
 
 #include <libgds/log.h>
 
-#define LOG_OUTPUT_NULL   0
-#define LOG_OUTPUT_STREAM 1
-#define LOG_OUTPUT_FILE   2
+#define LOG_STREAM_BUFFER_SIZE 1024
 
-SLog * pMainLog= NULL;
-
-// ----- log_create -------------------------------------------------
+// -----[ log_create_stream ]----------------------------------------
 /**
  *
  */
-SLog * log_create()
+SLogStream * log_create_stream(FILE * pStream)
 {
-  SLog * pLog= (SLog *) MALLOC(sizeof(SLog));
-  pLog->pStream= NULL;
-  pLog->uLevel= LOG_LEVEL_EVERYTHING;
-  pLog->uOutput= LOG_OUTPUT_NULL;
-  return pLog;
+  SLogStream * pLogStream= (SLogStream *) MALLOC(sizeof(SLogStream));
+  pLogStream->eLevel= LOG_LEVEL_EVERYTHING;
+  pLogStream->eType= LOG_TYPE_STREAM;
+  pLogStream->pStream= pStream;
+  return pLogStream;
+}
+
+// -----[ log_create_file ]-------------------------------------------
+/**
+ *
+ */
+SLogStream * log_create_file(char * pcFileName)
+{
+  SLogStream * pLogStream= NULL;
+  FILE * pFile;
+
+  if ((pFile= fopen(pcFileName, "w")) == NULL)
+    return NULL;
+
+  pLogStream= log_create_stream(pFile);
+  pLogStream->eType= LOG_TYPE_FILE;
+  return pLogStream;
+}
+
+// -----[ log_create_callback ]--------------------------------------
+/**
+ *
+ */
+SLogStream * log_create_callback(FLogStreamCallback fCallback,
+				 void * pContext)
+{
+  SLogStream * pLogStream= log_create_stream(stderr);
+
+  pLogStream->eType= LOG_TYPE_CALLBACK;
+  pLogStream->sCallback.fCallback= fCallback;
+  pLogStream->sCallback.pContext= pContext;
+  
+  return pLogStream;
 }
 
 // ----- log_destroy ------------------------------------------------
 /**
  *
  */
-void log_destroy(SLog ** ppLog)
+void log_destroy(SLogStream ** ppLogStream)
 {
-  if (*ppLog != NULL) {
-    if ((*ppLog)->uOutput == LOG_OUTPUT_FILE)
-    if ((*ppLog)->pStream != NULL)
-      fclose((*ppLog)->pStream);
-    FREE(*ppLog);
-    *ppLog= NULL;
+  if (*ppLogStream != NULL) {
+    if ((*ppLogStream)->eType == LOG_TYPE_FILE)
+      if ((*ppLogStream)->pStream != NULL)
+	fclose((*ppLogStream)->pStream);
+    FREE(*ppLogStream);
+    *ppLogStream= NULL;
   }
 }
 
@@ -54,34 +83,39 @@ void log_destroy(SLog ** ppLog)
 /**
  *
  */
-void log_set_level(SLog * pLog, uint8_t uLevel)
+void log_set_level(SLogStream * pLogStream, ELogLevel eLevel)
 {
-  pLog->uLevel= uLevel;
+  pLogStream->eLevel= eLevel;
 }
 
 // ----- log_get_stream ---------------------------------------------
 /**
  *
  */
+/*
 FILE * log_get_stream(SLog * pLog)
 {
   return pLog->pStream;
 }
+*/
 
 // ----- log_set_stream ---------------------------------------------
 /**
  *
  */
+/*
 void log_set_stream(SLog * pLog, FILE * pStream)
 {
   pLog->pStream= pStream;
   pLog->uOutput= LOG_OUTPUT_STREAM;
 }
+*/
 
 // ----- log_set_file -----------------------------------------------
 /**
  *
  */
+/*
 void log_set_file(SLog * pLog, char * pcFileName)
 {
   // Close previously opened file
@@ -96,55 +130,89 @@ void log_set_file(SLog * pLog, char * pcFileName)
   else
     perror("Unable to append to file");
 }
+*/
 
 // ----- log_enabled ------------------------------------------------
 /**
  *
  */
-int log_enabled(SLog * pLog, uint8_t uLevel)
+int log_enabled(SLogStream * pLogStream, ELogLevel eLevel)
 {
-  return (uLevel >= pLog->uLevel);
+  return (eLevel >= pLogStream->eLevel);
 }
 
-// ----- log_write --------------------------------------------------
+// ----- log_printf -------------------------------------------------
 /**
  *
  */
-void log_write(SLog * pLog, uint8_t uLevel, char * pcFormat, ...)
+void log_printf(SLogStream * pLogStream, char * pcFormat, ...)
 {
+  static char acBuffer[LOG_STREAM_BUFFER_SIZE];
   va_list ap;
 
-  if (pLog->uOutput != LOG_OUTPUT_NULL) {
-    assert(pLog->pStream != NULL);
-    
+  switch (pLogStream->eType) {
+  case LOG_TYPE_STREAM:
+  case LOG_TYPE_FILE:
+    assert(pLogStream->pStream != NULL);
     va_start(ap, pcFormat);
-    if (log_enabled(pLog, uLevel)) {
-      vfprintf(pLog->pStream, pcFormat, ap);
-      if ((uLevel > LOG_LEVEL_WARNING) &&
-	  (pLog->pStream != stderr))
-	vfprintf(stderr, pcFormat, ap);
-    }
+    vfprintf(pLogStream->pStream, pcFormat, ap);
+    /*if ((uLevel > LOG_LEVEL_WARNING) &&
+	(pLogStream->pStream != stderr))
+	vfprintf(stderr, pcFormat, ap);*/
+    break;
+
+  case LOG_TYPE_CALLBACK:
+    assert(pLogStream->sCallback.fCallback != NULL);
+    va_start(ap, pcFormat);
+    vsnprintf(acBuffer, LOG_STREAM_BUFFER_SIZE, pcFormat, ap);
+    pLogStream->sCallback.fCallback(pLogStream->sCallback.pContext, acBuffer);
+    break;
+
+  default:
+    abort();
   }
 }
+
+// -----[ log_flush ]------------------------------------------------
+/**
+ *
+ */
+void log_flush(SLogStream * pLogStream)
+{
+  switch (pLogStream->eType) {
+  case LOG_TYPE_STREAM:
+  case LOG_TYPE_FILE:
+    fflush(pLogStream->pStream);
+    break;
+  case LOG_TYPE_CALLBACK:
+    // TO BE IMPLEMENTED...
+    break;
+  default:
+    abort();
+  }
+}
+
 
 // ----- log_perror -------------------------------------------------
 /**
  *
  */
-void log_perror(SLog * pLog, char * pcFormat, ...)
+void log_perror(SLogStream * pLogStream, char * pcFormat, ...)
 {
   va_list ap;
 
   va_start(ap, pcFormat);
-  log_write(pLog, LOG_LEVEL_SEVERE, pcFormat, ap);
-  log_write(pLog, LOG_LEVEL_SEVERE, "%s\n", strerror(errno));
+  if (log_enabled(pLogStream, LOG_LEVEL_SEVERE)) {
+    log_printf(pLogStream, pcFormat, ap);
+    log_printf(pLogStream, "%s\n", strerror(errno));
+  }
 }
 
 // ----- log_str2level ----------------------------------------------
 /**
  *
  */
-uint8_t log_str2level(char * pcStr)
+ELogLevel log_str2level(char * pcStr)
 {
   if (strcasecmp(pcStr, "everything"))
     return LOG_LEVEL_EVERYTHING;
@@ -164,20 +232,29 @@ uint8_t log_str2level(char * pcStr)
 // INITIALIZATION AND FINALIZATION SECTION
 /////////////////////////////////////////////////////////////////////
 
+// -----[ Standard log streams ]-------------------------------------
+SLogStream * pLogErr= NULL;
+SLogStream * pLogOut= NULL;
+SLogStream * pLogDebug= NULL;
+
 // ----- _log_init --------------------------------------------------
 /**
- *
+ * Initialize "standard" log streams.
  */
 void _log_init()
 {
-  pMainLog= log_create();
+  pLogErr= log_create_stream(stderr);
+  pLogOut= log_create_stream(stdout);
+  pLogDebug= log_create_stream(stderr);
 }
 
 // ----- _log_destroy -----------------------------------------------
 /**
- *
+ * Destroy "standard" log streams.
  */
 void _log_destroy()
 {
-  log_destroy(&pMainLog);
+  log_destroy(&pLogErr);
+  log_destroy(&pLogOut);
+  log_destroy(&pLogDebug);
 }
