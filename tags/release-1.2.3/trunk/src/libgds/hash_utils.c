17185f075abc78e5f4389d7b82c64a2d03f7de99
vvandens <vvandens@8e8141a4-5d17-0410-8ace-bc53a4168d8f>
2006-10-30 15:29:14 +0000

    Creating release tag "release-1.2.3"

---
+++ tags/release-1.2.3/trunk/src/libgds/hash_utils.c
@@ -0,0 +1,31 @@
+// ==================================================================
+// @(#)hash.c
+//
+// @author Sebastien Tandel (standel@info.ucl.ac.be)
+// @date 14/12/2004
+// @lastdate 14/12/2004
+// ==================================================================
+
+#include <string.h>
+
+// ----- hash_utils_key_compute_string -------------------------------
+/**
+ * Universal hash function for string keys (discussed in Sedgewick's
+ * "Algorithms in C, 3rd edition") and adapted.
+ *
+ */
+int hash_utils_key_compute_string(char * pcItem, unsigned int uModulo)
+{
+  int iHash, a = 31415, b = 27183;
+  int iIndex;
+  
+  if (pcItem == NULL)
+    return 0;
+
+  iHash= 0;
+  for (iIndex= 0; iIndex < strlen(pcItem); iIndex++) {
+    iHash= (a*iHash+pcItem[iIndex]);
+    a= a*b%(uModulo-1);
+  }
+  return iHash;
+}
