a827e094a74b1607408f1092c87e562680ff4531
bquoitin <bquoitin@8e8141a4-5d17-0410-8ace-bc53a4168d8f>
2007-03-17 13:18:44 +0000

    New unit testing framework for libgds

---
+++ trunk/src/check/utest.h
@@ -0,0 +1,81 @@
+// ==================================================================
+// @(#)utest.h
+//
+// Generic Data Structures (libgds): unit testing framework.
+//
+// @author Bruno Quoitin (bqu@info.ucl.ac.be)
+// @author Sebastien Tandel (standel@info.ucl.ac.be)
+// @lastdate 17/03/2007
+// ==================================================================
+
+#ifndef __GDS_UTEST_H__
+#define __GDS_UTEST_H__
+
+#define TXT_BOLD       "\033[1m"
+#define TXT_COL_RED    "\033[31;1m"
+#define TXT_COL_GREEN  "\033[32;1m"
+#define TXT_COL_YELLOW "\033[33;1m"
+#define TXT_DEFAULT    "\033[0m"
+#define TXT_POS        "\033[65G"
+
+#define UTEST_SUCCESS 0
+#define UTEST_FAILURE -1
+#define UTEST_SKIPPED 1
+
+#ifdef __STDC_VERSION__
+#if (__STDC_VERSION__ >= 199901L)
+#define __VARIADIC_ELLIPSIS__
+#endif
+#endif
+
+#define MSG_RESULT_SUCCESS() \
+  printf("\033[70G[\033[32;1mSUCCESS\033[0m]\n")
+#define MSG_RESULT_FAIL() \
+  printf("\033[70G[\033[31;1mFAIL\033[0m]\n")
+
+#ifdef __VARIADIC_ELLIPSIS__
+#undef ASSERT_RETURN
+#define ASSERT_RETURN(TEST, ...) \
+  if (!(TEST)) { \
+    utest_set_message(__FILE__, __LINE__, __VA_ARGS__);	\
+    return -1; \
+  }
+#else
+#define ASSERT_RETURN(TEST, args...) \
+  if (!(TEST)) { \
+    utest_set_message(__FILE__,__LINE__, args);	\
+    return -1; \
+  }
+#endif
+
+typedef int (*FUnitTest)();
+typedef struct {
+  FUnitTest fTest;
+  char * pcName;
+  int iResult;
+  char * pcMessage;
+} SUnitTest;
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+  // -----[ utest_init ]---------------------------------------------
+  void utest_init();
+  // -----[ utest_done ]---------------------------------------------
+  void utest_done();
+  // -----[ utest_set_message ]--------------------------------------
+  void utest_set_message(const char * pcFile, int iLine,
+			 const char * pcFormat, ...);
+  // -----[ utest_run_suite ]----------------------------------------
+  int utest_run_suite(const char * pcName, SUnitTest * paTests,
+		      unsigned int uNumTests);
+
+  // -----[ utest_set_xml_logging ]----------------------------------
+  void utest_set_xml_logging(const char * pcFileName);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __GDS_UTEST_H__ */
