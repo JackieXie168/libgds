9279907629c1a08b2a31dfb4ff9991070c0e6e41
bquoitin <bquoitin@8e8141a4-5d17-0410-8ace-bc53a4168d8f>
2007-03-17 18:06:04 +0000

    Conversion functions now rely on code in str_util.h,.c

--- trunk/src/libgds/tokens.h
+++ trunk/src/libgds/tokens.h
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bqu@infonet.fundp.ac.be)
 // @date 20/08/2003
-// @lastdate 20/08/2003
+// @lastdate 16/01/2007
 // =================================================================
 
 #ifndef __GDS_TOKENS_H__
@@ -14,34 +14,41 @@
 
 typedef SPtrArray STokens;
 
-// ----- tokens_create ----------------------------------------------
-extern STokens * tokens_create();
-// ----- tokens_destroy ---------------------------------------------
-extern void tokens_destroy(STokens ** pTokens);
-// ----- tokens_add -------------------------------------------------
-extern int tokens_add(STokens * pTokens, char * pcToken);
-// ----- tokens_add_copy --------------------------------------------
-extern int tokens_add_copy(STokens * pTokens, char * pcToken);
-// ----- tokens_get_num ---------------------------------------------
-extern unsigned int tokens_get_num(STokens * pTokens);
-// ----- tokens_get_at ----------------------------------------------
-extern char * tokens_get_string_at(STokens * pTokens,
-				   unsigned int uIndex);
-// ----- tokens_get_long_at -----------------------------------------
-extern int tokens_get_long_at(STokens * pTokens, uint16_t uIndex,
-			      long int * plValue);
-// ----- tokens_get_int_at ------------------------------------------
-extern int tokens_get_int_at(STokens * pTokens, uint16_t uIndex,
-			     int * piValue);
-// ----- tokens_get_ulong_at ----------------------------------------
-extern int tokens_get_ulong_at(STokens * pTokens, uint16_t uIndex,
-			       unsigned long int * plValue);
-// ----- tokens_get_uint_at -----------------------------------------
-extern int tokens_get_uint_at(STokens * pTokens, uint16_t uIndex,
-			      unsigned int * puValue);
-// ----- tokens_get_double_at ---------------------------------------
-extern int tokens_get_double_at(STokens * pTokens,
-				uint16_t uIndex,
-				double * pdValue);
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+  // ----- tokens_create --------------------------------------------
+  STokens * tokens_create();
+  // ----- tokens_destroy -------------------------------------------
+  void tokens_destroy(STokens ** pTokens);
+  // ----- tokens_add -----------------------------------------------
+  int tokens_add(STokens * pTokens, char * pcToken);
+  // ----- tokens_add_copy ------------------------------------------
+  int tokens_add_copy(STokens * pTokens, char * pcToken);
+  // ----- tokens_get_num -------------------------------------------
+  unsigned int tokens_get_num(STokens * pTokens);
+  // ----- tokens_get_at --------------------------------------------
+  char * tokens_get_string_at(STokens * pTokens,
+				     unsigned int uIndex);
+  // ----- tokens_get_long_at ---------------------------------------
+  int tokens_get_long_at(STokens * pTokens, uint16_t uIndex,
+				long int * plValue);
+  // ----- tokens_get_int_at ----------------------------------------
+  int tokens_get_int_at(STokens * pTokens, uint16_t uIndex,
+			       int * piValue);
+  // ----- tokens_get_ulong_at --------------------------------------
+  int tokens_get_ulong_at(STokens * pTokens, uint16_t uIndex,
+				 unsigned long int * plValue);
+  // ----- tokens_get_uint_at ---------------------------------------
+  int tokens_get_uint_at(STokens * pTokens, uint16_t uIndex,
+				unsigned int * puValue);
+  // ----- tokens_get_double_at -------------------------------------
+  int tokens_get_double_at(STokens * pTokens, uint16_t uIndex,
+			   double * pdValue);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __GDS_TOKENS_H__ */
