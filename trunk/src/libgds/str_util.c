c0afaf5b14fc3a8b0d5f55dc59be5577449520aa
bquoitin <bquoitin@8e8141a4-5d17-0410-8ace-bc53a4168d8f>
2007-03-17 18:06:44 +0000

    Conversion functions from tokens.c,.h are moved here and updated

--- trunk/src/libgds/str_util.c
+++ trunk/src/libgds/str_util.c
@@ -3,12 +3,14 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 24/07/2003
-// @lastdate 27/01/2005
+// @lastdate 15/01/2007
 // =================================================================
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
+
+#include <errno.h>
 
 #include <libgds/memory.h>
 #include <libgds/str_util.h>
@@ -100,4 +102,101 @@ char * str_nappend(char ** ppcString, const char * pcStrToAppend,
     (*ppcString)[strlen(*ppcString)+tLen]= '\0';
   }
   return *ppcString;
+}
+
+// ----- str_as_long ------------------------------------------------
+/**
+ * Convert a string to a long (LONG_MIN <= X <= LONG_MAX).
+ */
+int str_as_long(const char * pcString, long int * plValue)
+{
+  long int lValue;
+  char * pcEndPtr;
+
+  if (plValue == NULL)
+    return -1;
+  lValue= strtol(pcString, &pcEndPtr, 0);
+  if (errno == ERANGE)
+    return -1;
+  *plValue= lValue;
+  return (*pcEndPtr == 0)?0:-1;
+}
+
+// ----- str_as_int -------------------------------------------------
+/**
+ * Convert a string to an integer (INT_MIN <= X <= INT_MAX).
+ */
+int str_as_int(const char * pcString, int * piValue)
+{
+  long int lValue;
+
+  if (piValue == NULL)
+    return -1;
+  if (str_as_long(pcString, &lValue) != 0)
+    return -1;
+  if ((lValue < INT_MIN) || (lValue > INT_MAX))
+    return -1;
+  *piValue= lValue;
+  return 0;
+}
+
+// ----- str_as_ulong -----------------------------------------------
+/**
+ * Convert a string to an unsigned long (0 <= X <= ULONG_MAX).
+ * 
+ * The function uses 'strtoll()' in order to convert the string to a
+ * 'long long int'. We cannot use 'strtol()' since it is limited to a
+ * maximum value of LONG_MAX while we need ULONG_MAX. We cannot use
+ * 'strtoul()' since it will convert a negative number to a positive
+ * number.
+ */
+int str_as_ulong(const char * pcString, unsigned long int * pulValue)
+{
+  long long int llValue;
+  char * pcEndPtr;
+
+  if (pulValue == NULL)
+    return -1;
+  llValue= strtoll(pcString, &pcEndPtr, 0);
+  /* Check for errors and bounds (0 <= x <= ULONG_MAX) */
+  if ((errno == ERANGE) || (llValue < 0) || (llValue > ULONG_MAX))
+    return -1;
+  *pulValue= (unsigned long int) llValue;
+  return (*pcEndPtr == 0)?0:-1;
+}
+
+// ----- str_as_uint ------------------------------------------------
+/**
+ * Convert a string to an unsigned int (0 < X <= UINT_MAX).
+ */
+int str_as_uint(const char * pcString, unsigned int * puValue)
+{
+  unsigned long int ulValue;
+
+  if (puValue == NULL)
+    return -1;
+  if (str_as_ulong(pcString, &ulValue) != 0)
+    return -1;
+  if (ulValue > UINT_MAX)
+    return -1;
+  *puValue= ulValue;
+  return 0;
+}
+
+// ----- str_as_double ----------------------------------------------
+/**
+ *
+ */
+int str_as_double(const char * pcString, double * pdValue)
+{
+  double dValue;
+  char * pcEndPtr;
+
+  if (pdValue == NULL)
+    return -1;
+  dValue= strtod(pcString, &pcEndPtr);
+  if (errno == ERANGE)
+    return -1;
+  *pdValue= dValue;
+  return (*pcEndPtr == 0)?0:-1;
 }
