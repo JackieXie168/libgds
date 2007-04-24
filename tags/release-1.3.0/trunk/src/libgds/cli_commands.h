3928c4924a516b803a526068bf0fc4d17c85d8b1
bquoitin <bquoitin@8e8141a4-5d17-0410-8ace-bc53a4168d8f>
2007-04-24 13:54:10 +0000

    Creating release tag "release-1.3.0"

---
+++ tags/release-1.3.0/trunk/src/libgds/cli_commands.h
@@ -0,0 +1,14 @@
+// ==================================================================
+// @(#)cli_commands.h
+//
+// @author Bruno Quoitin (bqu@info.ucl.ac.be)
+// @date 25/06/2003
+// @lastdate 05/01/2007
+// ==================================================================
+
+#ifndef __GDS_CLI_COMMANDS_H__
+#define __GDS_CLI_COMMANDS_H__
+
+
+
+#endif /* __GDS_CLI_COMMANDS_H__ */
