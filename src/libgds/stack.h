492c13ddc3bfd1ccb63ddee263eb6598056009d6
bquoitin <bquoitin@8e8141a4-5d17-0410-8ace-bc53a4168d8f>
2007-12-04 14:54:40 +0000

    Changes: - adapted to allow inclusion in C++ sources

--- src/libgds/stack.h
+++ src/libgds/stack.h
@@ -5,7 +5,7 @@
 //
 // @author Bruno Quoitin
 // @date 21/03/2003
-// @lastdate 18/07/2003
+// @lastdate 04/12/2007
 // ==================================================================
 
 #ifndef __STACK_H__
@@ -17,25 +17,33 @@ typedef struct {
   void * apItems[0];
 } SStack;
 
-// ----- stack_create -----------------------------------------------
-extern SStack * stack_create(int iMaxDepth);
-// ----- stack_destroy ----------------------------------------------
-extern void stack_destroy(SStack ** ppStack);
-// ----- stack_push -------------------------------------------------
-extern int stack_push(SStack * pStack, void * pItem);
-// ----- stack_pop --------------------------------------------------
-extern void * stack_pop(SStack * pStack);
-// ----- stack_top --------------------------------------------------
-extern void * stack_top(SStack * pStack);
-// ----- stack_get_at -----------------------------------------------
-extern void * stack_get_at(SStack * pStack, unsigned int iIndex);
-// ----- stack_depth ------------------------------------------------
-extern int stack_depth(SStack * pStack);
-// ----- stack_is_empty ---------------------------------------------
-extern int stack_is_empty(SStack * pStack);
-// ----- stack_copy -------------------------------------------------
-extern SStack * stack_copy(SStack * pStack);
-// ----- stack_compare ----------------------------------------------
-extern int stack_equal(SStack * pStack1, SStack * pStack2);
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+  
+  // ----- stack_create ---------------------------------------------
+  SStack * stack_create(int iMaxDepth);
+  // ----- stack_destroy --------------------------------------------
+  void stack_destroy(SStack ** ppStack);
+  // ----- stack_push -----------------------------------------------
+  int stack_push(SStack * pStack, void * pItem);
+  // ----- stack_pop ------------------------------------------------
+  void * stack_pop(SStack * pStack);
+  // ----- stack_top ------------------------------------------------
+  void * stack_top(SStack * pStack);
+  // ----- stack_get_at ---------------------------------------------
+  void * stack_get_at(SStack * pStack, unsigned int iIndex);
+  // ----- stack_depth ----------------------------------------------
+  int stack_depth(SStack * pStack);
+  // ----- stack_is_empty -------------------------------------------
+  int stack_is_empty(SStack * pStack);
+  // ----- stack_copy -----------------------------------------------
+  SStack * stack_copy(SStack * pStack);
+  // ----- stack_compare --------------------------------------------
+  int stack_equal(SStack * pStack1, SStack * pStack2);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STACK_H__ */
