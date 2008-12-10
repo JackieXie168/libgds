8beea4ad87305dec4b4fba7601e248460ef0091c
bquoitin <bquoitin@8e8141a4-5d17-0410-8ace-bc53a4168d8f>
2008-12-10 14:05:10 +0000

    Changes: - memalloc/memrealloc/memfree moved to header file as static inline.

--- trunk/src/libgds/memory.c
+++ trunk/src/libgds/memory.c
@@ -1,159 +1,83 @@
 // ==================================================================
 // @(#)memory.c
 //
-// @author Bruno Quoitin (bqu@info.ucl.ac.be), 
+// @author Bruno Quoitin (bruno.quoitin@uclouvain.be), 
 // @author Sebastien Tandel (standel@info.ucl.ac.be)
 // @date 17/05/2005
-// @lastdate 16/01/2007
+// $Id$
 // ==================================================================
-
-#include <stdio.h>
-#include <stdlib.h>
 
 #include <config.h>
 #include <libgds/memory.h>
 
 #include <libgds/memory_debug.h>
 
-// ----- uAllocCount -----
-/* uAllocCount tracks the balance of calls to memalloc/memfree. The
+/* 'alloc_count' tracks the balance of memalloc/memfree calls. The
  * allocation count is initialized to -1 and re-initialized to 0 by
  * the global ctor function _memory_init. If memalloc detects that the
  * allocation counter is -1, this means that the ctor function has not
  * yet been called (problem during linking process ?) */
-static long int dAllocCount= -1;
+static long int _alloc_count= -1;
+static uint8_t  _flags      = 0;
 
-// ----- uFlags -----
-static uint8_t uFlags= 0;
-
-// -----[ memalloc ]-------------------------------------------------
-/**
- *
- */
-void * memalloc(size_t size, char * pcFileName, int iLineNumber)
+// -----[ _mem_alloc_count_inc ]-------------------------------------
+void _mem_alloc_count_inc(const char * filename, int line_num)
 {
-  void * pNewPtr= malloc(size);
-
-  if (dAllocCount < 0) {
-    fprintf(stderr, "memalloc: dtor function _memory_init has not yet ");
-    fprintf(stderr, "been called. Check the linking process !!!\n");
-    exit(EXIT_FAILURE);
-  }
-  
-  if (pNewPtr == NULL) {
-    perror("memalloc: ");
-    exit(EXIT_FAILURE);
-  }
-
-#ifdef MEMORY_DEBUG
-  memory_debug_track_alloc(pNewPtr, size, pcFileName, iLineNumber);
-#endif
-  
-  dAllocCount++;
-  
-  return pNewPtr;
+  if (_alloc_count < 0)
+    gds_fatal("memalloc: dtor function _memory_init has not yet been called\n"
+	      "Check your linking process !!!\n");
 }
 
-// -----[ memrealloc ]-----------------------------------------------
-/**
- *
- */
-extern void * memrealloc(void * pPtr, size_t size,
-			 char * pcFileName, int iLineNumber)
+// -----[ _mem_alloc_count_dec ]-------------------------------------
+void _mem_alloc_count_dec(const char * filename, int line_num)
 {
-  void * pNewPtr= realloc(pPtr, size);
-
-  if (pNewPtr == NULL) {
-    perror("memrealloc: ");
-    exit(EXIT_FAILURE);
-  }
-    
-#ifdef MEMORY_DEBUG
-  memory_debug_track_realloc(pNewPtr, pPtr, size, pcFileName, iLineNumber);
-#endif
-
-  return pNewPtr;
+  if (_alloc_count <= 0)
+    gds_warn("memfree: alloc-count == %ld : %s (line %d)\n",
+	    _alloc_count, filename, line_num);
 }
 
-// -----[ memfree ]--------------------------------------------------
-/**
- *
- */
-void memfree(void * pPtr, char * pcFileName, int iLineNumber)
+// -----[ _mem_alloc_count_get ]-------------------------------------
+long int _mem_alloc_count_get()
 {
-#ifdef MEMORY_DEBUG
-  memory_debug_track_free(pPtr, pcFileName, iLineNumber);
-#endif
-
-  if (dAllocCount > 0) {
-    free(pPtr);
-  } else {
-#ifdef MEMORY_DEBUG
-    fprintf(stderr, "memfree: alloc-count == %ld : %s (line %d)\n",
-	    dAllocCount, pcFileName, iLineNumber);
-#endif
-  }
-  dAllocCount--;
-
+  return _alloc_count;
 }
 
-// ----- mem_alloc_cnt ----------------------------------------------
-/**
- *
- */
-long int mem_alloc_cnt()
+// -----[ mem_flag_set ]---------------------------------------------
+void mem_flag_set(uint8_t flag, int state)
 {
-  return dAllocCount;
-}
-
-// ----- mem_flag_set -----------------------------------------------
-/**
- *
- */
-void mem_flag_set(uint8_t uFlag, int iState)
-{
-  if (iState)
-    uFlags|= uFlag;
+  if (state)
+    _flags|= state;
   else
-    uFlags&= ~uFlag;
+    _flags&= ~state;
 }
 
-// ----- mem_flag_get -----------------------------------------------
-/**
- *
- */
-int mem_flag_get(uint8_t uFlag)
+// -----[ mem_flag_get ]---------------------------------------------
+int mem_flag_get(uint8_t flag)
 {
-  return (uFlags & uFlag);
+  return (_flags & flag);
 }
+
 
 /////////////////////////////////////////////////////////////////////
 // INITIALIZATION AND FINALIZATION FUNCTIONS
 /////////////////////////////////////////////////////////////////////
 
-// ----- _memory_init -----------------------------------------------
-/**
- *
- */
+// -----[ _memory_init ]---------------------------------------------
 void _memory_init()
 {
-  dAllocCount= 0;
-
-#ifdef MEMORY_DEBUG
+#ifdef GDS_MEMORY_DEBUG
+  _alloc_count= 0;
   memory_debug_init(mem_flag_get(MEM_FLAG_TRACK_LEAK));
-#endif
+#endif /* GDS_MEMORY_DEBUG */
 }
 
-// ----- _memory_destroy --------------------------------------------
-/**
- *
- */
+// -----[ _memory_destroy ]------------------------------------------
 void _memory_destroy()
 {
-  if (mem_flag_get(MEM_FLAG_WARN_LEAK) && (dAllocCount > 0))
-    fprintf(stderr, "WARNING: memory leak (%lu) !\n", dAllocCount);
+#ifdef GDS_MEMORY_DEBUG
+  if (mem_flag_get(MEM_FLAG_WARN_LEAK) && (_alloc_count > 0))
+    gds_warn("memory leak (%lu) !\n", _alloc_count);
 
-#ifdef MEMORY_DEBUG
   memory_debug_destroy();
 #endif
 }
