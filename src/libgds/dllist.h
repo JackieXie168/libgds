2d46ddc305fc4ef590476dcddbc2c221c3992323
bquoitin <bquoitin@8e8141a4-5d17-0410-8ace-bc53a4168d8f>
2009-03-27 11:44:33 +0000

    Changes: - cosmetic changes

--- src/libgds/dllist.h
+++ src/libgds/dllist.h
@@ -3,51 +3,83 @@
 //
 // Doubly-linked lists
 //
-// @author Bruno Quoitin (bqu@info.ucl.ac.be)
+// @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @date 26/07/2006
-// @lastdate 29/07/2006
+// $Id$
 // ==================================================================
 
-#ifndef __DLLIST_H__
-#define __DLLIST_H__
+/**
+ * \file
+ * Provide data structures and functions to manage doubly-linked
+ * lists.
+ */
 
-// -----[ SDLListItem ]----------------------------------------------
-struct TDLListItem {
-  struct TDLListItem * pPrevious;
-  struct TDLListItem * pNext;
-  void * pUserData;
-};
-typedef struct TDLListItem SDLListItem;
+#ifndef __GDS_DLLIST_H__
+#define __GDS_DLLIST_H__
 
-// -----[ FDLListForEach ]-------------------------------------------
-typedef int (* FDLListForEach)(void * pUserData, void * pContext);
-// -----[ FDLListDestroy ]-------------------------------------------
-typedef void (* FDLListDestroy)(SDLListItem ** ppItem);
+// -----[ gds_dllist_item_t ]----------------------------------------
+typedef struct gds_dllist_item_t {
+  struct gds_dllist_item_t * prev;
+  struct gds_dllist_item_t * next;
+  void * user_data;
+} gds_dllist_item_t;
 
-// -----[ SDLList ]--------------------------------------------------
+// -----[ gds_dllist_foreach_f ]-------------------------------------
+/** Callback function used to traverse a list. */
+typedef int  (*gds_dllist_foreach_f)(void * data, void * ctx);
+
+// -----[ gds_dllist_destroy_f ]-------------------------------------
+/** Callback function used to destroy an item in a list. */
+typedef void (*gds_dllist_destroy_f)(void * user_data);
+
+// -----[ gds_dllist_t ]---------------------------------------------
+/**
+ * Definition of a doubly-linked list.
+ */
 typedef struct {
-  SDLListItem * pRoot;
-  FDLListDestroy fDestroy;
-} SDLList;
+  gds_dllist_item_t    * root;
+  gds_dllist_destroy_f   destroy;
+} gds_dllist_t;
 
-// -----[ dllist_create ]--------------------------------------------
-extern SDLList * dllist_create();
-// -----[ dllist_destroy ]-------------------------------------------
-extern void dllist_destroy(SDLList ** ppList);
-// -----[ dllist_insert ]--------------------------------------------
-extern int dllist_insert(SDLList * pList, unsigned int uIndex,
-			 void * pUserData);
-// -----[ dllist_remove ]--------------------------------------------
-extern int dllist_remove(SDLList * pList, unsigned int uIndex);
-// -----[ dllist_append ]--------------------------------------------
-extern int dllist_append(SDLList * pList, void * pUserData);
-// -----[ dllist_get ]-----------------------------------------------
-extern int dllist_get(SDLList * pList, unsigned int uIndex,
-		      void ** ppUserData);
-// -----[ dllist_size ]-----------------------------------------------
-extern unsigned int dllist_size(SDLList * pList);
-// -----[ dllist_for_each ]------------------------------------------
-extern int dllist_for_each(SDLList * pList, void * pContext,
-			   FDLListForEach fForEach);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#endif /* __DLLIST_H__ */
+  // -----[ dllist_create ]------------------------------------------
+  /**
+   * Create a doubly-linked list.
+   */
+  gds_dllist_t * dllist_create(gds_dllist_destroy_f destroy);
+
+  // -----[ dllist_destroy ]-----------------------------------------
+  /**
+   * Destroy a doubly-linked list.
+   */
+  void dllist_destroy(gds_dllist_t ** list_ref);
+
+  // -----[ dllist_insert ]------------------------------------------
+  int dllist_insert(gds_dllist_t * list, unsigned int index,
+		    void * data);
+
+  // -----[ dllist_remove ]------------------------------------------
+  int dllist_remove(gds_dllist_t * list, unsigned int index);
+
+  // -----[ dllist_append ]------------------------------------------
+  void dllist_append(gds_dllist_t * list, void * data);
+
+  // -----[ dllist_get ]---------------------------------------------
+  int dllist_get(gds_dllist_t * list, unsigned int index,
+		 void ** data_ref);
+
+  // -----[ dllist_size ]--------------------------------------------
+  unsigned int dllist_size(gds_dllist_t * list);
+
+  // -----[ dllist_for_each ]----------------------------------------
+  int dllist_for_each(gds_dllist_t * list, void * ctx,
+		      gds_dllist_foreach_f foreach);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __GDS_DLLIST_H__ */
