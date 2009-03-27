277f8a0d8948c4c64af8a663d42ec6c26c9b6b45
bquoitin <bquoitin@8e8141a4-5d17-0410-8ace-bc53a4168d8f>
2009-03-27 11:58:09 +0000

    Changes: - cosmetic changes

--- trunk/src/libgds/dllist.c
+++ trunk/src/libgds/dllist.c
@@ -3,9 +3,9 @@
 //
 // Doubly-linked lists
 //
-// @author Bruno Quoitin (bqu@info.ucl.ac.be)
+// @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @date 26/07/2006
-// @lastdate 30/07/2006
+// $Id$
 // ==================================================================
 
 #ifdef HAVE_CONFIG_H
@@ -15,53 +15,59 @@
 #include <libgds/dllist.h>
 #include <libgds/memory.h>
 
-// -----[ dllist_item_create ]---------------------------------------
-SDLListItem * dllist_item_create(void * pUserData,
-				 SDLListItem * pPrevious,
-				 SDLListItem * pNext)
+// -----[ _dllist_item_create ]--------------------------------------
+static inline
+gds_dllist_item_t * _dllist_item_create(void * user_data,
+					gds_dllist_item_t * prev,
+					gds_dllist_item_t * next)
 {
-  SDLListItem * pItem= (SDLListItem *) MALLOC(sizeof(SDLListItem));
-  pItem->pUserData= pUserData;
-  pItem->pPrevious= pPrevious;
-  pItem->pNext= pNext;
-  return pItem;
+  gds_dllist_item_t * item=
+    (gds_dllist_item_t *) MALLOC(sizeof(gds_dllist_item_t));
+  item->user_data= user_data;
+  item->prev= prev;
+  item->next= next;
+  return item;
 }
 
-// -----[ dllist_item_destroy ]--------------------------------------
-void dllist_item_destroy(SDLListItem ** ppItem, FDLListDestroy fDestroy)
+// -----[ _dllist_item_destroy ]-------------------------------------
+static inline
+void _dllist_item_destroy(gds_dllist_item_t ** item_ref,
+			  gds_dllist_destroy_f destroy)
 {
-  if (*ppItem != NULL) {
-    if (fDestroy != NULL)
-      fDestroy(ppItem);
-    FREE(*ppItem);
-    *ppItem= NULL;
-  }
+  gds_dllist_item_t * item= *item_ref;
+  if (item == NULL)
+    return;
+  if (destroy != NULL)
+    destroy(item->user_data);
+  FREE(item);
+  *item_ref= NULL;
 }
 
 // -----[ dllist_create ]--------------------------------------------
-SDLList * dllist_create(FDLListDestroy fDestroy)
+gds_dllist_t * dllist_create(gds_dllist_destroy_f destroy)
 {
-  SDLList * pList= (SDLList *) MALLOC(sizeof(SDLList));
-  pList->pRoot= NULL;
-  pList->fDestroy= fDestroy;
-  return pList;
+  gds_dllist_t * list= (gds_dllist_t *) MALLOC(sizeof(gds_dllist_t));
+  list->root= NULL;
+  list->destroy= destroy;
+  return list;
 }
 
 // -----[ dllist_destroy ]-------------------------------------------
-void dllist_destroy(SDLList ** ppList)
+void dllist_destroy(gds_dllist_t ** list_ref)
 {
-  SDLListItem * pItem, *pItemTmp;
+  gds_dllist_item_t * item, * tmp;
+  gds_dllist_t * list= *list_ref;
 
-  if (*ppList != NULL) {
-    pItem= (*ppList)->pRoot;
-    while (pItem != NULL) {
-      pItemTmp= pItem;
-      pItem= pItem->pNext;
-      dllist_item_destroy(&pItemTmp, (*ppList)->fDestroy);
-    }
-    FREE(*ppList);
-    *ppList= NULL;
+  if (list == NULL)
+    return;
+  item= list->root;
+  while (item != NULL) {
+    tmp= item;
+    item= item->next;
+    _dllist_item_destroy(&tmp, list->destroy);
   }
+  FREE(list);
+  *list_ref= NULL;
 }
 
 // -----[ dllist_insert ]--------------------------------------------
@@ -70,24 +76,24 @@ void dllist_destroy(SDLList ** ppList)
  *   0 success
  *   -1 invalid index
  */
-int dllist_insert(SDLList * pList, unsigned int uIndex, void * pUserData)
+int dllist_insert(gds_dllist_t * list, unsigned int index,
+		  void * user_data)
 {
-  SDLListItem * pItem;
+  gds_dllist_item_t * item;
 
-  if (uIndex == 0) {
-    pList->pRoot= dllist_item_create(pUserData, NULL, pList->pRoot);
-  } else {
-    pItem= pList->pRoot;
-    while ((pItem != NULL) && (uIndex > 1)) {
-      pItem= pItem->pNext;
-      uIndex--;
-    }
-    if (pItem != NULL) {
-      pItem->pNext= dllist_item_create(pUserData, pItem, pItem->pNext);
-    } else {
-      return -1;
-    }
+  if (index == 0) {
+    list->root= _dllist_item_create(user_data, NULL, list->root);
+    return 0;
   }
+
+  item= list->root;
+  while ((item != NULL) && (index > 1)) {
+    item= item->next;
+    index--;
+  }
+  if (item == NULL)
+    return -1;
+  item->next= _dllist_item_create(user_data, item, item->next);
   return 0;
 }
 
@@ -97,97 +103,94 @@ int dllist_insert(SDLList * pList, unsigned int uIndex, void * pUserData)
  *   0  success
  *   -1 invalid index
  */
-int dllist_remove(SDLList * pList, unsigned int uIndex)
+int dllist_remove(gds_dllist_t * list, unsigned int index)
 {
-  SDLListItem * pTmpItem; // Pointer to item to delete
-  SDLListItem ** ppCurrItem; // Pointer to pointer of item to delete
+  gds_dllist_item_t * tmp; // Pointer to item to delete
+  gds_dllist_item_t ** item_ref; // Pointer to pointer of item to delete
 
   // Not even a single item, abort with error
-  if (pList->pRoot == NULL)
+  if (list->root == NULL)
     return -1;
 
-  ppCurrItem= &(pList->pRoot);
-  while ((uIndex > 0) && (*ppCurrItem != NULL)) {
+  item_ref= &(list->root);
+  while ((index > 0) && (*item_ref != NULL)) {
     // Note: advance pointer to pointer
-    ppCurrItem= &(*ppCurrItem)->pNext;
-    uIndex--;
+    item_ref= &(*item_ref)->next;
+    index--;
   }
-  if (*ppCurrItem == NULL)
+  if (*item_ref == NULL)
     return -1;
 
   // Keep reference to current item
-  pTmpItem= *ppCurrItem;
+  tmp= *item_ref;
 
   // Update cross-references
-  if ((*ppCurrItem)->pNext != NULL)
-    (*ppCurrItem)->pNext->pPrevious= (*ppCurrItem)->pPrevious;
-  *ppCurrItem= (*ppCurrItem)->pNext;
+  if ((*item_ref)->next != NULL)
+    (*item_ref)->next->prev= (*item_ref)->prev;
+  *item_ref= (*item_ref)->next;
 
   // Destroy temporary item
-  dllist_item_destroy(&pTmpItem, pList->fDestroy);
-
+  _dllist_item_destroy(&tmp, list->destroy);
   return 0;
 }
 
 // -----[ dllist_append ]--------------------------------------------
-/**
- */
-int dllist_append(SDLList * pList, void * pUserData)
+void dllist_append(gds_dllist_t * list, void * user_data)
 {
-  SDLListItem * pItem;
+  gds_dllist_item_t * item;
 
-  if (pList->pRoot == NULL) {
-    pList->pRoot= dllist_item_create(pUserData, NULL, NULL);
-  } else {
-    pItem= pList->pRoot;
-    while (pItem->pNext != NULL) {
-      pItem= pItem->pNext;
-    }
-    pItem->pNext= dllist_item_create(pUserData, pItem, NULL);
+  if (list->root == NULL) {
+    list->root= _dllist_item_create(user_data, NULL, NULL);
+    return;
   }
-  return 0;
+
+  item= list->root;
+  while (item->next != NULL)
+    item= item->next;
+  item->next= _dllist_item_create(user_data, item, NULL);
 }
 
 // -----[ dllist_get ]-----------------------------------------------
-int dllist_get(SDLList * pList, unsigned int uIndex, void ** ppUserData)
+int dllist_get(gds_dllist_t * list, unsigned int index,
+	       void ** user_data_ref)
 {
-  SDLListItem * pItem= pList->pRoot;
+  gds_dllist_item_t * item= list->root;
 
-  while ((uIndex > 0) && (pItem != NULL)) {
-    pItem= pItem->pNext;
-    uIndex--;
+  while ((index > 0) && (item != NULL)) {
+    item= item->next;
+    index--;
   }
-  if (pItem == NULL)
+  if (item == NULL)
     return -1;
-  *ppUserData= pItem->pUserData;
+  *user_data_ref= item->user_data;
   return 0;
 }
 
 // -----[ dllist_size ]-----------------------------------------------
-unsigned int dllist_size(SDLList * pList)
+unsigned int dllist_size(gds_dllist_t * list)
 {
-  SDLListItem * pItem= pList->pRoot;
-  unsigned int uSize= 0;
+  gds_dllist_item_t * item= list->root;
+  unsigned int size= 0;
 
-  while (pItem != NULL) {
-    pItem= pItem->pNext;
-    uSize++;
+  while (item != NULL) {
+    item= item->next;
+    size++;
   }
-  return uSize;
+  return size;
 }
 
 // -----[ dllist_for_each ]------------------------------------------
-int dllist_for_each(SDLList * pList, void * pContext,
-		    FDLListForEach fForEach)
+int dllist_for_each(gds_dllist_t * list, void * ctx,
+		    gds_dllist_foreach_f foreach)
 {
-  SDLListItem * pItem= pList->pRoot;
-  int iResult;
+  gds_dllist_item_t * item= list->root;
+  int result;
 
-  while (pItem != NULL) {
-    iResult= fForEach(pItem->pUserData, pContext);
-    if (iResult != 0)
-      return iResult;
-    pItem= pItem->pNext;
+  while (item != NULL) {
+    result= foreach(item->user_data, ctx);
+    if (result != 0)
+      return result;
+    item= item->next;
   }
   return 0;
 }
