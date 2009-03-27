f659d8561a28aeedb00a728f9eaf49b0bf5760e6
bquoitin <bquoitin@8e8141a4-5d17-0410-8ace-bc53a4168d8f>
2009-03-27 11:45:40 +0000

    Changes: - cosmetic changes - documentation

--- trunk/src/libgds/tokens.c
+++ trunk/src/libgds/tokens.c
@@ -16,7 +16,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <str_util.h>
-#include <tokens.h>
+
+#include <libgds/memory.h>
+#include <libgds/tokens.h>
 
 // -----[ _tokens_item_destroy ]-------------------------------------
 static void _tokens_item_destroy(void * item, const void * ctx)
@@ -24,136 +26,126 @@ static void _tokens_item_destroy(void * item, const void * ctx)
   str_destroy((char **) item);
 }
 
-// ----- tokens_create ----------------------------------------------
-/**
- *
- */
+// -----[ tokens_create ]--------------------------------------------
 gds_tokens_t * tokens_create()
 {
-  return (gds_tokens_t *) ptr_array_create(0, NULL,
-					   _tokens_item_destroy, NULL);
+  gds_tokens_t * tks= (gds_tokens_t *) MALLOC(sizeof(gds_tokens_t));
+  tks->num_tokens= 0;
+  tks->array= ptr_array_create(0, NULL, _tokens_item_destroy, NULL);
+  return tks;
 }
 
-// ----- tokens_destroy ---------------------------------------------
-/**
- *
- */
-void tokens_destroy(gds_tokens_t ** tokens_ref)
+// -----[ tokens_destroy ]-------------------------------------------
+void tokens_destroy(gds_tokens_t ** tks_ref)
 {
-  ptr_array_destroy((SPtrArray **) tokens_ref);
+  gds_tokens_t * tks= *tks_ref;
+  if (tks != NULL) {
+    ptr_array_destroy(&tks->array);
+    FREE(tks);
+    *tks_ref= NULL;
+  }
 }
 
-// ----- tokens_add -------------------------------------------------
-/**
- *
- */
-int tokens_add(const gds_tokens_t * tokens, char * token)
+// -----[ tokens_clear ]---------------------------------------------
+void tokens_clear(gds_tokens_t * tks)
 {
-  return ptr_array_add((SPtrArray *) tokens, &token);
+  tks->num_tokens= 0;
 }
 
-// ----- tokens_add_copy --------------------------------------------
-/**
- *
- */
-int tokens_add_copy(const gds_tokens_t * tokens, char * token)
+// -----[ tokens_add ]-----------------------------------------------
+int tokens_add(gds_tokens_t * tks, char * token)
 {
-  char * token_copy= str_create(token);
-  return ptr_array_add((SPtrArray *) tokens, &token_copy);
+  int result;
+
+  // No need to extend array length, insert at end
+  if (tks->num_tokens < ptr_array_length(tks->array)) {
+    FREE(tks->array->data[tks->num_tokens]);
+    tks->array->data[tks->num_tokens]= token;
+    result= tks->num_tokens;
+  } else {
+    result= ptr_array_add(tks->array, &token);
+  }
+  if (result < 0)
+    return -1;
+  tks->num_tokens++;
+  return result;
 }
 
-// ----- tokens_get_num ---------------------------------------------
+// -----[ tokens_add_copy ]------------------------------------------
 /**
  *
  */
-unsigned int tokens_get_num(const gds_tokens_t * tokens)
+int tokens_add_copy(gds_tokens_t * tks, const char * token)
 {
-  return ptr_array_length((SPtrArray *) tokens);
+  return tokens_add(tks, str_create(token));
 }
 
-// ----- tokens_get_string_at ---------------------------------------
-/**
- *
- */
-char * tokens_get_string_at(const gds_tokens_t * tokens, unsigned int index)
+// -----[ tokens_get_num ]-------------------------------------------
+unsigned int tokens_get_num(const gds_tokens_t * tks)
 {
-  if (index < tokens_get_num(tokens))
-    return (char *) tokens->data[index];
-  return NULL;
+  return tks->num_tokens;
 }
 
-// ----- tokens_get_long_at -----------------------------------------
-/**
- *
- */
-int tokens_get_long_at(const gds_tokens_t * tokens, unsigned int index,
+// -----[ tokens_get_string_at ]-------------------------------------
+const char * tokens_get_string_at(const gds_tokens_t * tks,
+				  unsigned int index)
+{
+  if (index >= tokens_get_num(tks))
+    return NULL;
+  return (char *) tks->array->data[index];
+}
+
+// -----[ tokens_get_long_at ]---------------------------------------
+int tokens_get_long_at(const gds_tokens_t * tks, unsigned int index,
 		       long int * value)
 {
-  if (index >= tokens_get_num(tokens))
+  if (index >= tokens_get_num(tks))
     return -1;
-  if (str_as_long(tokens->data[index], value) < 0)
+  if (str_as_long(tks->array->data[index], value) < 0)
     return -1;
   return 0;
 }
 
-// ----- tokens_get_int_at ------------------------------------------
-/**
- *
- */
-int tokens_get_int_at(const gds_tokens_t * tokens, unsigned int index,
+// -----[ tokens_get_int_at ]----------------------------------------
+int tokens_get_int_at(const gds_tokens_t * tks, unsigned int index,
 		      int * value)
 {
-  if (index >= tokens_get_num(tokens))
+  if (index >= tokens_get_num(tks))
     return -1;
-  if (str_as_int(tokens->data[index], value) < 0)
+  if (str_as_int(tks->array->data[index], value) < 0)
     return -1;
   return 0;
 }
 
-// ----- tokens_get_ulong_at ----------------------------------------
-/**
- * The function uses 'strtoll()' in order to convert the string to a
- * 'long long int'.
- *
- * Note: we cannot use 'strtol()' since it is limited to a maximum
- * value of LONG_MAX while we need ULONG_MAX. We cannot use
- * 'strtoul()' since it will convert a negative number to a positive
- * number.
- */
-int tokens_get_ulong_at(const gds_tokens_t * tokens, unsigned int index,
+// -----[ tokens_get_ulong_at ]--------------------------------------
+int tokens_get_ulong_at(const gds_tokens_t * tks, unsigned int index,
 			unsigned long int * value)
 {
-  if (index >= tokens_get_num(tokens))
+  if (index >= tokens_get_num(tks))
     return -1;
-  if (str_as_ulong(tokens->data[index], value) < 0)
+  if (str_as_ulong(tks->array->data[index], value) < 0)
     return -1;
   return 0;
 }
 
-// ----- tokens_get_uint_at -----------------------------------------
-/**
- *
- */
-int tokens_get_uint_at(const gds_tokens_t * tokens, unsigned int index,
+// -----[ tokens_get_uint_at ]---------------------------------------
+int tokens_get_uint_at(const gds_tokens_t * tks, unsigned int index,
 		       unsigned int * value)
 {
-  if (index >= tokens_get_num(tokens))
+  if (index >= tokens_get_num(tks))
     return -1;
-  if (str_as_uint(tokens->data[index], value) < 0)
+  if (str_as_uint(tks->array->data[index], value) < 0)
     return -1;
   return 0;
 }
 
-// ----- tokens_get_double_at ---------------------------------------
-/**
- *
- */
-int tokens_get_double_at(const gds_tokens_t * tokens, unsigned int index,
+// -----[ tokens_get_double_at ---------------------------------------
+int tokens_get_double_at(const gds_tokens_t * tks, unsigned int index,
 			 double * value)
 {
-  if (index >= tokens_get_num(tokens))
+  if (index >= tokens_get_num(tks))
     return -1;
-  if (str_as_double(tokens->data[index], value) < 0)
+  if (str_as_double(tks->array->data[index], value) < 0)
     return -1;
   return 0;
 }
