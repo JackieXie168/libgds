baee5abd71f7dd78635303aa2527d97c8ce3785f
bquoitin <bquoitin@8e8141a4-5d17-0410-8ace-bc53a4168d8f>
2009-03-27 11:49:19 +0000

    Changes: - cosmetic changes - documentation

--- src/libgds/str_util.h
+++ src/libgds/str_util.h
@@ -6,6 +6,12 @@
 // $Id$
 // =================================================================
 
+/**
+ * \file
+ * Provide a set of functions to manipulate and convert
+ * NUL-terminated strings.
+ */
+
 #ifndef __GDS_STR_UTIL_H__
 #define __GDS_STR_UTIL_H__
 
@@ -23,29 +29,73 @@ typedef struct str_buf_t {
 extern "C" {
 #endif
 
-  // -----[ strsep ]--------------------------------------------------
 #ifndef strsep
-  char * strsep(char ** ppcStr, const char * pcDelim);
+  // -----[ strsep ]--------------------------------------------------
+  /**
+   * This is a replacement for \c strsep() in case it is not provided
+   * by the system. See strsep's man page for more information.
+   */
+  char * strsep(char ** str, const char * delim);
 #endif
 
-  // ----- str_lcreate ----------------------------------------------
+  // -----[ str_lcreate ]--------------------------------------------
+  /**
+   * Create a string of N characters.
+   *
+   * Allocates memory for N characters + 1 character for the
+   * terminating NUL-character.
+   *
+   * \param len is the number of characters (len+1 will be allocated).
+   */
   char * str_lcreate(size_t len);
-  // ----- str_create -----------------------------------------------
+
+  // -----[ str_create ]---------------------------------------------
+  /**
+   * Create a copy of a string.
+   *
+   * \param s is the string to be copied.
+   */
   char * str_create(const char * s);
-  // ----- str_ncreate ----------------------------------------------
+
+  // -----[ str_ncreate ]--------------------------------------------
+  /**
+   * Create a copy of the N first characters of a string.
+   *
+   * Allocates memory for N charecters + 1 character for the
+   * terminating NUL-character. If the length of the source string
+   * is less than N, N+1 characters are also allocated.
+   *
+   * \param s   is the string to be copied.
+   * \param len is the number of characters to be copied.
+   */
   char * str_ncreate(const char * s, size_t len);
-  // ----- str_lextend ----------------------------------------------
-  char * str_lextend(char ** s, size_t new_len);
-  // ----- str_destroy ----------------------------------------------
-  void str_destroy(char ** s);
-  // ----- str_append -----------------------------------------------
-  char * str_append(char ** s,
-		    const char * append);
-  // ----- str_nappend ----------------------------------------------
-  char * str_nappend(char ** s, const char * append, size_t len);
-  // ----- str_prepend ----------------------------------------------
-  char * str_prepend(char ** s, const char * prepend);
-  // ----- str_translate --------------------------------------------
+
+  // -----[ str_lextend ]--------------------------------------------
+  /**
+   * Change the length of a string.
+   *
+   * \param s       is the string to update.
+   * \param new_len is the new string length.
+   * \retval the new string (might be different from the initial
+   *   pointer).
+   */
+  char * str_lextend(char * s, size_t new_len);
+
+  // -----[ str_destroy ]--------------------------------------------
+  /**
+   * Destroy a string.
+   *
+   * \param s_ref is a pointer to the string to be destroyed.
+   */
+  void str_destroy(char ** s_ref);
+
+  // -----[ str_append ]---------------------------------------------
+  char * str_append(char * s, const char * append);
+  // -----[ str_nappend ]--------------------------------------------
+  char * str_nappend(char * s, const char * append, size_t len);
+  // -----[ str_prepend ]--------------------------------------------
+  char * str_prepend(char * s, const char * prepend);
+  // -----[ str_translate ]------------------------------------------
   void str_translate(char * s, const char * src_chars,
 		     const char * dst_chars);
 
@@ -54,15 +104,15 @@ extern "C" {
   // STRING CONVERSION FUNCTIONS
   ///////////////////////////////////////////////////////////////////
 
-  // ----- str_as_long ----------------------------------------------
+  // -----[ str_as_long ]--------------------------------------------
   int str_as_long(const char * s, long int * value);
-  // ----- str_as_int -----------------------------------------------
+  // -----[ str_as_int ]---------------------------------------------
   int str_as_int(const char * s, int * value);
-  // ----- str_as_ulong ---------------------------------------------
+  // -----[ str_as_ulong ]-------------------------------------------
   int str_as_ulong(const char * s, unsigned long int * value);
-  // ----- str_as_uint ----------------------------------------------
+  // -----[ str_as_uint ]--------------------------------------------
   int str_as_uint(const char * s, unsigned int * value);
-  // ----- str_as_double --------------------------------------------
+  // -----[ str_as_double ]------------------------------------------
   int str_as_double(const char * s, double * value);
 
 
