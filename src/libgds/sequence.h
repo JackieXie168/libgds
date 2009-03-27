11f49c7aa74909a83f2f3ef9e84c7617c1140c8b
bquoitin <bquoitin@8e8141a4-5d17-0410-8ace-bc53a4168d8f>
2009-03-27 11:50:16 +0000

    Changes: - cosmetic changes - documentation

--- src/libgds/sequence.h
+++ src/libgds/sequence.h
@@ -1,61 +1,156 @@
 // ==================================================================
 // @(#)sequence.h
 //
-// @author Bruno Quoitin (bqu@infonet.fundp.ac.be)
+// @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @date 23/11/2002
-// @lastdate 04/12/2002
+// $Id$
 // ==================================================================
 
-#ifndef __SEQUENCE_H__
-#define __SEQUENCE_H__
+/**
+ * \file
+ * Provide data structure and functions to manipulate a sequence of
+ * items.
+ */
 
-extern unsigned long sequence_create_count;
-extern unsigned long sequence_copy_count;
-extern unsigned long sequence_destroy_count;
+#ifndef __GDS_SEQUENCE_H__
+#define __GDS_SEQUENCE_H__
 
+/** Compare two items in the sequence. */
+typedef int (*gds_seq_cmp_f)(const void * item1, const void * item2);
 
-// ----- FSeqCompare -----------------------------------------------
-typedef int (*FSeqCompare)(void * pItem1, void * pItem2);
+/** Destroy an item in the sequence. */
+typedef void (*gds_seq_destroy_f)(void ** item_ref);
 
-// ----- FSeqDestroy -----------------------------------------------
-typedef void (*FSeqDestroy)(void ** ppItem);
+/** Callback used to traverse the whole sequence. */
+typedef int (*gds_seq_foreach_f)(const void * item, void * ctx);
 
-// ----- FSeqForEach -----------------------------------------------
-typedef void (*FSeqForEach)(void * pItem, void * pContext);
+/** Copy an item. */
+typedef void * (*gds_seq_copy_f)(void * item);
 
-// ----- FSeqCopyItem ----------------------------------------------
-typedef void * (*FSeqCopyItem)(void * pItem);
-
-// ----- SSequence -------------------------------------------------
+// -----[ gds_seq_t ]------------------------------------------------
 typedef struct {
-  int iSize;
-  void ** ppItems;
-  FSeqCompare fCompare;
-  FSeqDestroy fDestroy;
-} SSequence;
+  unsigned int         size;
+  void              ** items;
+  gds_seq_cmp_f        cmp;
+  gds_seq_destroy_f    destroy;
+} gds_seq_t;
 
-// ----- sequence_create --------------------------------------------
-extern SSequence * sequence_create(FSeqCompare fCompare,
-				   FSeqDestroy fDestroy);
-// ----- sequence_destroy -------------------------------------------
-extern void sequence_destroy(SSequence ** ppSequence);
-// ----- sequence_find_index ----------------------------------------
-extern int sequence_find_index(SSequence * pSequence, void * pItem);
-// ----- sequence_insert_index --------------------------------------
-extern int sequence_insert_at(SSequence * pSequence, int iIndex,
-			      void * pItem);
-// ----- sequence_add -----------------------------------------------
-extern int sequence_add(SSequence * pSequence, void * pItem);
-// ----- sequence_remove --------------------------------------------
-extern int sequence_remove(SSequence * pSequence, void * pItem);
-// ----- sequence_for_each ------------------------------------------
-extern void sequence_for_each(SSequence * pSequence,
-			      FSeqForEach fForEach,
-			      void * pContext);
-// ----- sequence_copy ----------------------------------------------
-extern SSequence * sequence_copy(SSequence * pSequence,
-				 FSeqCopyItem fCopyItem);
-// ----- sequence_remove_at -----------------------------------------
-extern int sequence_remove_at(SSequence * pSequence, int iIndex);
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+  // -----[ sequence_create ]----------------------------------------
+  /**
+   * Create a sequence.
+   *
+   * \param cmp     is an item comparison function (can be NULL).
+   * \param destroy is an item destruction function (can be NULL).
+   */
+  gds_seq_t * sequence_create(gds_seq_cmp_f cmp, gds_seq_destroy_f destroy);
+
+  // -----[ sequence_destroy ]---------------------------------------
+  /**
+   * Destroy a sequence.
+   *
+   * If non-NULL, will call the destroy callback on each item.
+   *
+   * \param seq_ref is a pointer to the sequence.
+   */
+  void sequence_destroy(gds_seq_t ** seq_ref);
+
+  // -----[ sequence_find_index ]------------------------------------
+  /**
+   * Find the index of an item in the sequence.
+   *
+   * The comparison callback is used to find the item. If the
+   * comparison callback is NULL, the pointer value of item is
+   * compared to the pointer values in the sequence.
+   *
+   * \param seq       is the target sequence.
+   * \param item      is the searched item.
+   * \param index_ref is the pointer to the returned index
+   * \retval 0 if found,
+   *         <0 otherwise
+   */
+  int sequence_index_of(gds_seq_t * seq, void * item,
+			unsigned int * index_ref);
+
+  // -----[ sequence_insert_at ]-------------------------------------
+  /**
+   * Insert an item at a given position in the sequence.
+   *
+   * \param seq   is the target sequence.
+   * \param index is the insertion position [0, size].
+   * \param item  is the item to be inserted.
+   */
+  void sequence_insert_at(gds_seq_t * seq, unsigned int index,
+			  void * item);
+
+  // -----[ sequence_add ]-------------------------------------------
+  /**
+   * Add an item to the sequence.
+   *
+   * The item is inserted at the end of the sequence.
+   *
+   * \param seq  is the target sequence.
+   * \param item is the item to be inserted.
+   */
+  void sequence_add(gds_seq_t * seq, void * item);
+
+  // -----[ sequence_remove ]----------------------------------------
+  /**
+   * Remove an item from the sequence.
+   *
+   * The comparison callback is used to find the item. If the
+   * comparison callback is NULL, the pointer value of item is
+   * compared to the pointer values in the sequence. The destroy
+   * callback is called to free the removed item.
+   *
+   * \param seq  is the target sequence.
+   * \param item is the item to be removed.
+   * \retval 0 in case of success,
+   *         <0 otherwise
+   */
+  int sequence_remove(gds_seq_t * seq, void * item);
+
+  // ----- sequence_remove_at -----------------------------------------
+  /**
+   * Remove an item at a given position in the sequence.
+   *
+   * \param seq   is the target sequence.
+   * \param index is the location of the item to be removed.
+   */
+  void sequence_remove_at(gds_seq_t * seq, unsigned int index);
+
+  // -----[ sequence_for_each ]--------------------------------------
+  /**
+   * Call a specific function for each item in the sequence.
+   *
+   * \param seq     is the target sequence.
+   * \param foreach is the callback function.
+   * \param ctx     is the callback context pointer.
+   * \retval 0 in case of success,
+   *         <0 otherwise
+   */
+  int sequence_for_each(gds_seq_t * seq, gds_seq_foreach_f foreach,
+			void * ctx);
+
+  // -----[ sequence_copy ]----------------------------------------------
+  /**
+   * Duplicate a sequence and its content.
+   *
+   * The items added to the new sequence are produced by the provided
+   * copy function.
+   *
+   * \param seq  is the original sequence.
+   * \param copy is the copy function.
+   * \retval a new sequence
+   */
+  gds_seq_t * sequence_copy(gds_seq_t * seq,
+			    gds_seq_copy_f copy);
+  
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __GDS_SEQUENCE_H__ */
